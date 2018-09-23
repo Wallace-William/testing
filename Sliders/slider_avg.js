83359fbcf721f7e45a6979fa2d09eb7d489eb30c
Wallace-William <williamleewallace97@gmail.com>
2018-09-22 23:34:25 -0500

    store avg in local storage

--- Sliders/slider_avg.js
+++ Sliders/slider_avg.js
@@ -1,8 +1,9 @@
 function avg() {
 
   var avg_output = document.getElementById("slider_avg");
-  var avg_val = (parseFloat(slider.value) + parseFloat(slider2.value) + parseFloat(slider3.value) + parseFloat(slider4.value)) / 4;
+  avg_val = (parseFloat(slider.value) + parseFloat(slider2.value) + parseFloat(slider3.value) + parseFloat(slider4.value)) / 4;
 
+  localStorage.setItem('avg',avg_val);
   color(avg_output,avg_val);
 
   avg_output.innerHTML = avg_val;
